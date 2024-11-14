3bda00e14de3bef39ee75dc94246d2797266fcac
a <kuykendallmaustin@gmail.com>
2024-11-14 13:50:36 -0800

    debugging why email wont change on event listener console in JS file

--- JavaScript/validation.js
+++ JavaScript/validation.js
@@ -1,10 +1,14 @@
 // the name of the form element is in the input tag
 // be careful with naming conventions and the actual naming convention
 
-const { log } = require("handlebars");
+
 
 // "ev" is predifned in javaScript
 document.getElementById('Username').addEventListener('change', function(ev){
+    console.log(ev);
+})
+
+document.getElementById('email').addEventListener('change', function(){
     console.log(ev);
 })
 document.getElementById('password').addEventListener('change', function(ev){
@@ -12,10 +16,6 @@ document.getElementById('password').addEventListener('change', function(ev){
 })
 
 document.getElementById('checkPass').addEventListener('change', function(){
-    console.log(ev);
-})
-
-document.getElementById('email').addEventListener('change', function(){
     console.log(ev);
 })
 
